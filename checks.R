ff88b2d8e585a920b4e246b76a08f649fa72d876
Your Name <you@example.com>
2026-06-21 06:25:40 +0200

    cleanup and improvements

--- checks.R
+++ checks.R
@@ -1,14 +1,40 @@
+# Maintenance Routines for the ephys4 Family ---------------------------------
+
 library(dplyr)
-list.dirs(recursive = FALSE)
 github_repos <- dir(pattern = "git*", full.names = TRUE)
 
+
+## Status for all repos -------------------------------------------------------
+for (repo in github_repos) {
+  message(">>> ", repo)
+  system2("git", c("-C", repo, "status"), stdout = TRUE)->msg
+  if(msg[4]=="nothing to commit, working tree clean"){
+    cli::cli_inform(c("v"=msg[4]))
+  }else{
+    cli::cli_inform(c("x"=msg[4]))
+  }
+  
+}
+
+
+
+
+## Pull all repos --------------------------------------------------------------
+for (repo in github_repos) {
+  message(">>> ", repo)
+  system2("git", c("-C", repo, "pull"))
+}
+
+
+
+
+## Check the Remotes: field in all DESCRIPTION files ---------------------------
 
 desc_files <- list.files(
   recursive = TRUE, pattern = "DESCRIPTION",
   path = github_repos, full.names = TRUE
 )
 
-# check the Remotes field in all DESCRIPTION files
 desc_files |>
   lapply(\(f) {
     dcf <- read.dcf(f, fields = "Remotes")
@@ -18,12 +44,14 @@ desc_files |>
   }) |>
   bind_rows()
 
-# check the Install tipp in  all README files
+
+
+
+## Check the install tipp in  all README files  -------------------------------
 readme_files <- list.files(
   recursive = TRUE, pattern = "README.Rmd",
   path = github_repos, full.names = TRUE
 )
-
 
 readme_files |>
   lapply(\(f) {
@@ -35,28 +63,21 @@ readme_files |>
   bind_rows()
 
 
-# pull all repos
-for (repo in github_repos) {
-  message(">>> ", repo)
-  system2("git", c("-C", repo, "pull"))
-}
-
-system("git config --global user.name 'timm'")
-system("git config --global user.email 'tim@me.de'")
 
 
-# Git add, commit, push all repos
-commit_msg <- shQuote("fixed Remotes in DESCRIPTION")
+## Commit to all repos (discouraged)-------------------------------------------
+#system("git config --global user.name 'timm'")
+#system("git config --global user.email 'tim@me.de'")
 
-for (repo in github_repos) {
-  message(">>> ", repo)
-  system2("git", c("-C", repo, "add", "-A"))
-  system2("git", c("-C", repo, "commit", "-m", commit_msg))
-  system2("git", c("-C", repo, "push"))
-}
-
-# status for all repos
-for (repo in github_repos) {
-  message(">>> ", repo)
-  system2("git", c("-C", repo, "status"))
-}
+# if(FALSE){
+#   # Git add, commit, push all repos
+#   commit_msg <- shQuote("fixed Remotes in DESCRIPTION")
+#   
+#   for (repo in github_repos) {
+#     message(">>> ", repo)
+#     #system2("git", c("-C", repo, "add", "-A"))
+#     #system2("git", c("-C", repo, "commit", "-m", commit_msg))
+#     #system2("git", c("-C", repo, "push"))
+#   }
+# }
+# 
