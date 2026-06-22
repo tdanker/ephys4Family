c1974263f08e8ef80368734b9e3bd717d0ae07c9
tdanker <tidafr@uber.space>
2026-06-22 11:09:26 +0200

    script to clone the subrepos

---
+++ get_subrepos.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/NMIephys/ephys4R github_ephys4R
+git clone https://github.com/NMIephys/ephys4Patchmaster github_ephys4Patchmaster
+git clone https://github.com/NMIephys/ephys4Roboo github_ephys4Roboo
+git clone https://github.com/NMIephys/ephys4HiClamp github_ephys4HiClamp
+git clone https://github.com/NMIephys/ephys4HAMAMATSU github_ephys4HAMAMATZU
+git clone https://github.com/NMIephys/NMItemplates github_NMItemplates
+git clone https://github.com/tdanker/ggscalebars github_ggscalebars
