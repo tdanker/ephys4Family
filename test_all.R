54786d590e15a81aebce2764b71d8a592682ff00
Your Name <you@example.com>
2026-06-22 17:56:59 +0200

    .

--- test_all.R
+++ test_all.R
@@ -6,13 +6,33 @@ lib <- tempfile()
 dir.create(lib)
 
 
-withr::with_libpaths(lib,action = "prefix",  {
+withr::with_libpaths(lib,action = "replace",  {
   
-  devtools::install(".", upgrade="never", quick=TRUE)
+  install.packages("pak")   # required for devtools::install
+  install.packages("callr") # required for devtools::install
   
-  devtools::test("../ephys4HAMA")
-  devtools::test("../ephys4Patchmaster")
-  devtools::test("../ephys4Roboo")
-  #devtools::test("../ephys4HiClamp")
+  devtools::install("github_ephys4Patchmaster/", 
+                    upgrade=TRUE, 
+                    quick=TRUE, 
+                    dependencies = TRUE #this is required to also install Suggests like ephysdata. 
+                    )
+  
+  
+  devtools::test("github_ephys4Patchmaster/") #ok 22.6.2026
+  
+  
+})
+
+
+withr::with_libpaths(lib,action = "replace",  {
+  
+  # WARNING: 
+  # These two are not declared in Suggests, but needed in tests (will be removed soon). 
+  devtools::install("github_ephys4HAMAMATZU/", quick=TRUE)  
+  devtools::install("github_ephys4Roboo/", quick=TRUE)
+  
+  
+  devtools::test("github_ephys4R/")    #ok 22.6.2026
+  
   
 })
